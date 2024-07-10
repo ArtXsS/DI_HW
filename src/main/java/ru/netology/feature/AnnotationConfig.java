3f49166bf78b6b922f14b9717169abab08e3b035
ArtXs <arti06tube@gmail.com>
2024-07-10 13:33:39 +0400

    Initial commit

---
+++ src/main/java/ru/netology/feature/AnnotationConfig.java
@@ -0,0 +1,9 @@
+package ru.netology.feature;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "ru.netology")
+public class AnnotationConfig {
+}
---
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,49 @@
+package ru.netology.servlet;
+
+import org.springframework.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.netology.controller.PostController;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class MainServlet extends HttpServlet {
+    private PostController controller;
+    @Autowired
+    public MainServlet(PostController controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+
+            if (method.equals("GET") && path.equals("/api/posts")) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals("POST") && path.equals("/api/posts")) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
