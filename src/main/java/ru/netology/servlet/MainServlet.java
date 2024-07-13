4094c6d996fec03ddf304bc507e5daa96b04070c
ArtXs <arti06tube@gmail.com>
2024-07-13 20:42:45 +0400

    Sec Commit

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,19 +1,43 @@
 package ru.netology.servlet;
 
-import org.springframework.*;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import ru.netology.controller.PostController;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@Component
+@Configuration
+@ComponentScan
 public class MainServlet extends HttpServlet {
     private PostController controller;
-    @Autowired
-    public MainServlet(PostController controller) {
-        this.controller = controller;
+    private static final String GET_METHOD = "GET";
+    private static final String POST_METHOD = "POST";
+
+    @Bean
+    public PostController postController(PostService service) {
+        return new PostController(service);
+    }
+
+    @Bean
+    public PostService postService(PostRepository repository) {
+        return new PostService(repository);
+    }
+
+    @Bean
+    public PostRepository postRepository() {
+        return new PostRepository();
+    }
+
+    @Override
+    public void init() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MainServlet.class);
+        controller = context.getBean(PostController.class);
     }
 
     @Override
@@ -22,16 +46,16 @@ public class MainServlet extends HttpServlet {
             final var path = req.getRequestURI();
             final var method = req.getMethod();
 
-            if (method.equals("GET") && path.equals("/api/posts")) {
+            if (method.equals(GET_METHOD) && path.equals("/api/posts")) {
                 controller.all(resp);
                 return;
             }
-            if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
+            if (method.equals(GET_METHOD) && path.matches("/api/posts/\\d+")) {
                 final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
                 controller.getById(id, resp);
                 return;
             }
-            if (method.equals("POST") && path.equals("/api/posts")) {
+            if (method.equals(POST_METHOD) && path.equals("/api/posts")) {
                 controller.save(req.getReader(), resp);
                 return;
             }
